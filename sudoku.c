3395355c325fed8f62c715debd4cd9bb672e55c9
Your Name <you@example.com>
2024-05-16 01:22:00 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -53,7 +53,7 @@ int is_valid(Node* n){
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
-    for(int i = 1; i < 9; i++)
+    for(int i = 0; i < 9; i++)
        {
           for(int j = 0; j < 9; j++)
              {
