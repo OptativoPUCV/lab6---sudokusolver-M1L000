24300e2397c477bfca0f93ab02b54409351db218
Your Name <you@example.com>
2024-05-16 03:28:41 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -70,13 +70,18 @@ int is_valid(Node* n){
                      return 0;
                   }
                   
-               }
-
-               
+               }   
                
             }
          hacerCeroArr(arr);
-      }   
+      }  
+   int k=4,p; 
+   for(p=0;p<9;p++){
+       int i=3*(k/3) + (p/3) ;
+       int j=3*(k%3) + (p%3) ;
+       printf("%d ",n->sudo[i][j]);
+       if(p%3 == 2) printf("\n");
+   }
 
    
    
